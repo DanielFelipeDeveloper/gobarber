9ab6a8b8792eb6751a061bc5dab0e3d9bf7f4240
Daniel Developer <danielfelipedeveloper@gmail.com>
2020-12-29 16:26:58 -0400

    feat: criando classe de erro

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,6 +3,8 @@ import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -21,11 +23,11 @@ class AuthenticateUserService {
 
     const user = await usersRepository.findOne({ where: { email } });
 
-    if (!user) throw new Error('Incorrect email/password');
+    if (!user) throw new AppError('Incorrect email/password', 401);
 
     const passwordMatched = await compare(password, user.password);
 
-    if (!passwordMatched) throw new Error('Incorrect email/password');
+    if (!passwordMatched) throw new AppError('Incorrect email/password', 401);
 
     const { secret, expiresIn } = authConfig.jwt;
 
