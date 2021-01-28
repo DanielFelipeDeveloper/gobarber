2be77339ff1bbdd0c9e2fcd09db6cf6b13cdeea6
Daniel Developer <danielfelipedeveloper@gmail.com>
2021-01-28 10:37:31 -0400

    feat: estilos globais

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
